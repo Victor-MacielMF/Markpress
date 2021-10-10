9fd775f5d7aa1aac588b2b07fbf96461d7f4e629
Victor-MacielMF <joaovictormacieldefreitas2345@gmail.com>
2021-10-10 16:10:55 -0300

    aa

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,3 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
-
-        <x-jet-validation-errors class="mb-4" />
-
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
@@ -39,5 +31,3 @@
                 </x-jet-button>
             </div>
         </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
